0673650ab80c27cc0ca920fd9c6b8559f7017476
Deepika Kumari <chaudharydeepika210@gmail.com>
2023-06-04 14:10:42 +0530

    conlict check

--- Task.py
+++ Task.py
@@ -1,3 +1,4 @@
 print ('Hello World!')
 print ('Task Repo_1 succesfully created')
-print ('Changes commited in Git Repository')
+print ('Changes commited in Git Repository example for conflict')
+print('Merge changes')
